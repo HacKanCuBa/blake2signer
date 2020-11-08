HacKanCuBa/blake2signer

commit 66ebeff465206bd4b7f6b2596582108bd710f486

:art: Rename hashers choices enum
Additionally make it a string subclass so instead of
using the enum one can use a string directly.

blake2signer/serializers.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 
 from . import errors
 from .signers import Blake2TimestampSignerBase
-from .signers import Hashers_
+from .signers import HasherChoice
 from .utils import b64decode
 from .utils import b64encode
 
@@ -111,7 +111,7 @@ class Blake2SerializerSignerBase(Blake2TimestampSignerBase, ABC):
         max_age: typing.Union[None, int, float, timedelta] = None,
         personalisation: bytes = b'',
         digest_size: typing.Optional[int] = None,
-        hasher: Hashers_ = Hashers_.blake2b,
+        hasher: HasherChoice = HasherChoice.blake2b,
     ) -> None:
         """Serialize, sign and verify serialized signed data using Blake2.
 
@@ -396,7 +396,7 @@ class Blake2SerializerSigner(
         max_age: typing.Union[None, int, float, timedelta] = None,
         personalisation: bytes = b'',
         digest_size: typing.Optional[int] = None,
-        hasher: Hashers_ = Hashers_.blake2b,
+        hasher: HasherChoice = HasherChoice.blake2b,
         serializer: typing.Type[SerializerInterface] = JSONSerializer,
         compressor: typing.Type[CompressorInterface] = ZlibCompressor,
         encoder: typing.Type[EncoderInterface] = B64URLEncoder,

blake2signer/signers.py
@@ -34,8 +34,8 @@ class TimestampedDataParts:
     timestamp: bytes
 
 
-class Hashers_(Enum):
-    """Hasher selection options."""
+class HasherChoice(str, Enum):
+    """Hasher selection choices."""
 
     blake2b = 'blake2b'
     blake2s = 'blake2s'
@@ -44,7 +44,7 @@ class Hashers_(Enum):
 class Blake2SignerBase(ABC):
     """Base class for a signer based on Blake2 in keyed hashing mode."""
 
-    Hashers = Hashers_
+    Hashers = HasherChoice
 
     MIN_SECRET_SIZE: int = 16
     MIN_DIGEST_SIZE: int = 16
@@ -57,7 +57,7 @@ class Blake2SignerBase(ABC):
         *,
         personalisation: bytes = b'',
         digest_size: typing.Optional[int] = None,
-        hasher: Hashers_ = Hashers.blake2b,
+        hasher: HasherChoice = HasherChoice.blake2b,
     ) -> None:
         """Sign and verify signed data using Blake2 in keyed hashing mode.
 

blake2signer/tests/test_signers.py
@@ -8,7 +8,6 @@ from unittest import mock
 from .. import errors
 from ..signers import Blake2Signer
 from ..signers import Blake2TimestampSigner
-from ..signers import Hashers_
 
 
 class Blake2SignerTests(TestCase):
@@ -29,7 +28,7 @@ class Blake2SignerTests(TestCase):
         signer = Blake2Signer(self.secret)
         self.assertIsInstance(signer, Blake2Signer)
 
-        signer = Blake2Signer(self.secret, hasher=Hashers_.blake2s)
+        signer = Blake2Signer(self.secret, hasher=Blake2Signer.Hashers.blake2s)
         self.assertIsInstance(signer, Blake2Signer)
 
     def test_sign_unsign_all_options(self) -> None:
@@ -115,7 +114,11 @@ class Blake2SignerErrorTests(TestCase):
             Blake2Signer(self.secret, digest_size=4)
 
         with self.assertRaises(errors.InvalidOptionError):
-            Blake2Signer(self.secret, digest_size=4, hasher=Hashers_.blake2s)
+            Blake2Signer(
+                self.secret,
+                digest_size=4,
+                hasher=Blake2Signer.Hashers.blake2s,
+            )
 
     def test_digest_too_large(self) -> None:
         """Test digest too large."""
@@ -123,7 +126,11 @@ class Blake2SignerErrorTests(TestCase):
             Blake2Signer(self.secret, digest_size=65)
 
         with self.assertRaises(errors.InvalidOptionError):
-            Blake2Signer(self.secret, digest_size=33, hasher=Hashers_.blake2s)
+            Blake2Signer(
+                self.secret,
+                digest_size=33,
+                hasher=Blake2Signer.Hashers.blake2s,
+            )
 
     def test_unsign_wrong_data(self) -> None:
         """Test unsign with wrong data."""
@@ -183,7 +190,10 @@ class Blake2TimestampSignerTests(TestCase):
         signer = Blake2TimestampSigner(self.key)
         self.assertIsInstance(signer, Blake2TimestampSigner)
 
-        signer = Blake2TimestampSigner(self.key, hasher=Hashers_.blake2s)
+        signer = Blake2TimestampSigner(
+            self.key,
+            hasher=Blake2TimestampSigner.Hashers.blake2s,
+        )
         self.assertIsInstance(signer, Blake2TimestampSigner)
 
     def test_sign(self) -> None:

commit 3c0f1ef02cf096eafa73663ac7f0ae391d845997

:bug: Fix hasher option can be string
Introduced by 66ebeff as an idea but the
implementation was wrong/missing.

blake2signer/serializers.py
@@ -111,7 +111,7 @@ class Blake2SerializerSignerBase(Blake2TimestampSignerBase, ABC):
         max_age: typing.Union[None, int, float, timedelta] = None,
         personalisation: bytes = b'',
         digest_size: typing.Optional[int] = None,
-        hasher: HasherChoice = HasherChoice.blake2b,
+        hasher: typing.Union[HasherChoice, str] = HasherChoice.blake2b,
     ) -> None:
         """Serialize, sign and verify serialized signed data using Blake2.
 
@@ -396,7 +396,7 @@ class Blake2SerializerSigner(
         max_age: typing.Union[None, int, float, timedelta] = None,
         personalisation: bytes = b'',
         digest_size: typing.Optional[int] = None,
-        hasher: HasherChoice = HasherChoice.blake2b,
+        hasher: typing.Union[HasherChoice, str] = HasherChoice.blake2b,
         serializer: typing.Type[SerializerInterface] = JSONSerializer,
         compressor: typing.Type[CompressorInterface] = ZlibCompressor,
         encoder: typing.Type[EncoderInterface] = B64URLEncoder,

blake2signer/signers.py
@@ -1,12 +1,11 @@
 """Signers: low level classes to sign data."""
 
+import hashlib
 import typing
 from abc import ABC
 from dataclasses import dataclass
 from datetime import timedelta
 from enum import Enum
-from hashlib import blake2b
-from hashlib import blake2s
 from secrets import compare_digest
 from secrets import token_urlsafe
 from time import time
@@ -57,7 +56,7 @@ class Blake2SignerBase(ABC):
         *,
         personalisation: bytes = b'',
         digest_size: typing.Optional[int] = None,
-        hasher: HasherChoice = HasherChoice.blake2b,
+        hasher: typing.Union[HasherChoice, str] = Hashers.blake2b,
     ) -> None:
         """Sign and verify signed data using Blake2 in keyed hashing mode.
 
@@ -79,8 +78,11 @@ class Blake2SignerBase(ABC):
                                 to bytes.
         :raise InvalidOptionError: A parameter is out of bounds.
         """
-        self._hasher: typing.Union[typing.Type[blake2b], typing.Type[blake2s]]
-        self._hasher = blake2b if hasher is self.Hashers.blake2b else blake2s
+        self._hasher: typing.Union[
+            typing.Type[hashlib.blake2b],
+            typing.Type[hashlib.blake2s],
+        ]
+        self._hasher = self._choose_hasher(hasher)
 
         secret = self._force_bytes(secret)
         person = self._force_bytes(personalisation)
@@ -106,6 +108,21 @@ class Blake2SignerBase(ABC):
     def _salt_size(self) -> int:
         """Get the salt size."""
         return self._hasher.SALT_SIZE
+
+    def _choose_hasher(
+        self,
+        hasher: typing.Union[HasherChoice, str],
+    ) -> typing.Union[typing.Type[hashlib.blake2b], typing.Type[hashlib.blake2s]]:
+        """Choose hashing function."""
+        if hasher == self.Hashers.blake2b:
+            return hashlib.blake2b
+        elif hasher == self.Hashers.blake2s:
+            return hashlib.blake2s
+
+        raise errors.InvalidOptionError(
+            f'invalid hasher choice, must be one of: '
+            f'{", ".join(h for h in self.Hashers)}',
+        )
 
     def _derive_person(self, person: bytes) -> bytes:
         """Derive given personalisation value to ensure it fits the hasher correctly."""

blake2signer/tests/test_signers.py
@@ -1,5 +1,6 @@
 """Signers module tests."""
 
+import hashlib
 from datetime import datetime
 from datetime import timedelta
 from unittest import TestCase
@@ -27,9 +28,11 @@ class Blake2SignerTests(TestCase):
         """Test correct class defaults initialisation."""
         signer = Blake2Signer(self.secret)
         self.assertIsInstance(signer, Blake2Signer)
+        self.assertIs(signer._hasher, hashlib.blake2b)
 
         signer = Blake2Signer(self.secret, hasher=Blake2Signer.Hashers.blake2s)
         self.assertIsInstance(signer, Blake2Signer)
+        self.assertIs(signer._hasher, hashlib.blake2s)
 
     def test_sign_unsign_all_options(self) -> None:
         """Test correct signing and unsigning using all options."""
@@ -92,6 +95,14 @@ class Blake2SignerTests(TestCase):
 
         self.assertEqual(len(signed1), len(signed2))
         self.assertNotEqual(signed1, signed2)
+
+    def test_initialisation_hasher_as_string(self) -> None:
+        """Test initialisation with hasher as string."""
+        signer = Blake2Signer(self.secret, hasher='blake2b')
+        self.assertIs(signer._hasher, hashlib.blake2b)
+
+        signer = Blake2Signer(self.secret, hasher='blake2s')
+        self.assertIs(signer._hasher, hashlib.blake2s)
 
 
 class Blake2SignerErrorTests(TestCase):
@@ -175,6 +186,11 @@ class Blake2SignerErrorTests(TestCase):
         with self.assertRaises(errors.ConversionError):
             # noinspection PyTypeChecker
             Blake2Signer(self.secret).unsign(1.0)  # type: ignore
+
+    def test_wrong_hasher_choice(self) -> None:
+        """Test wrong hasher choice."""
+        with self.assertRaises(errors.InvalidOptionError):
+            Blake2Signer(self.secret, hasher='blake2')
 
 
 class Blake2TimestampSignerTests(TestCase):